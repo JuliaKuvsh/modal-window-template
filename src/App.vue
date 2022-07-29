3120e7b817c32bd0ce2bb67593b2534c04e4048f
JuliaKuvsh <juliakachergina@mail.ru>
2022-07-29 13:32:52 +0300

    Добавлен шаблон модального окна

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+    <div id="App">
+        <!-- Вешаем обработчик на кнопку для открытия модального окна -->
+        <button class="button" @click = "showModal">Открыть окно</button>
+        <!-- Используем ref, чтобы затем обратиться к модальному окну -->
+        <ModalWindow ref="modal"></ModalWindow>
+    </div>
+</template>
+
+<script>
+
+import ModalWindow from './components/ModalWindow.vue';
+
+export default {
+    components: {
+        'ModalWindow': ModalWindow,
+    },
+
+    data() {
+        return {
+        }
+    },
+
+    methods: {
+        showModal () {
+            this.$refs.modal.isModalVisible = true; //Показываем модальное окно при нажатии на кнопку
+        },
+    }
+}
+</script>
+
+
+<style>
+
+    body {
+        font-family: 'Source Sans Pro', sans-serif;
+        margin: 0;
+        padding: 0;
+        height: 100%;
+    }
+ 
+    #App {
+        position: relative;
+        width: 100%;
+        min-height: 100%;
+    }
+
+    .button {
+        background-color: #0971c7;
+        color: #fff;
+        border: none;
+        text-align: center;
+        padding: 8px;
+        font-size: 17px;
+        font-weight: 500;
+        border-radius: 8px;
+        min-width: 150px;
+    }
+
+</style>
---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,86 @@
+<template>
+    <!-- Используем директиву v-show, чтобы скрывать и показывать модальное окно -->
+    <div v-show="isModalVisible" class="modal-window" id="draggable" autofocus>
+        <div class="modal-window__header">
+            <h2 class="modal-window__title">
+                <!-- Создаем слоты под добавление произвольного контента -->
+                <slot>{{header}}</slot>
+            </h2>
+            <!-- Добавляем обработчик клика на кнопку -->
+            <button class="modal-window__close" @click ="closeModal" >&#10006;</button>
+        </div>
+        <div class="modal-window__content">
+            <!-- Создаем слоты под добавление произвольного контента -->
+            <slot>{{content}}</slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ModalWindow",
+    data() {
+        return {
+            isModalVisible: false, //Задаем модальному окну скрытое значение по умолчанию
+            header: 'Заголовок',
+            content: 'Текст модального окна'
+        }
+    },
+    methods: {
+     closeModal () {
+       this.isModalVisible = false; //Скрываем модальное окно при нажатии на кнопку
+     }
+
+    }
+}
+</script>
+
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+}
+
+.modal-window {
+    border: 2px solid #c5d0e6;
+    background: #fff;
+    border-radius: 8px;
+    padding: 15px;
+    min-width: 420px;
+    max-width: 480px;
+    position: absolute;
+    top: 50%;
+    left: 35%;
+}
+
+.modal-window__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.modal-window__close {
+    border-radius: 50%;
+    color: #fff;
+    background: #2a4cc7;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 7px;
+    right: 7px;
+    width: 30px;
+    height: 30px;
+    cursor: pointer;
+}
+
+.modal-window__title {
+    color: #0971c7;
+}
+
+.modal-window__content {
+    margin-top: 20px;
+}
+
+</style>
